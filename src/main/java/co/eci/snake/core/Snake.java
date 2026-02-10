52a0f726831afac54bc6a191cc65a893e7bf80c7
Nestor Lopez <nestor.lopez-c@mail.escuelaing.edu.co>
2026-02-10 07:23:43 -0500

    fix: fixing class Snake

--- src/main/java/co/eci/snake/core/Snake.java
+++ src/main/java/co/eci/snake/core/Snake.java
@@ -29,11 +29,11 @@ public final class Snake {
     this.direction = dir;
   }
 
-  public Position head() { return body.peekFirst(); }
+  public synchronized Position head() { return body.peekFirst(); }
 
-  public Deque<Position> snapshot() { return new ArrayDeque<>(body); }
+  public synchronized Deque<Position> snapshot() { return new ArrayDeque<>(body); }
 
-  public void advance(Position newHead, boolean grow) {
+  public synchronized void advance(Position newHead, boolean grow) {
     body.addFirst(newHead);
     if (grow) maxLength++;
     while (body.size() > maxLength) body.removeLast();
